2f30b45cbb5c5988926fb22d1fe94a3aa91c1de6
Nikita <tiurikovnikita@gmail.com>
2023-07-03 09:13:34 +0300

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'o4a9s85OEN',
-        'HOST': 'localhost',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
         'PORT': '',
     }
 }
